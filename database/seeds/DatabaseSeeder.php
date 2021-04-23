3b5b76a0cc1b0c00134c8dc0c0fa9fa086ef76d4
Masashi <t15519ms@gmail.com>
2021-04-23 21:29:55 +0900

    DB関連。Booksテーブルの作成とデータ登録。

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        // BooksTableSeederを読み込むように指定
+        $this->call(BooksTableSeeder::class);
     }
 }
